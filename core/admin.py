3b1aef8373cd7a37418f1ce5ef5ef3315653fabf
Saul Santeliz <santelizdev@gmail.com>
2026-02-24 12:10:35 -0300

    Add new column to activation_code of view device django

--- core/admin.py
+++ core/admin.py
@@ -1,4 +1,5 @@
 # core/admin.py
+from django.contrib import admin
 from .admin_configs import (  # noqa: F401
     client,
     branch,
@@ -9,3 +10,17 @@ from .admin_configs import (  # noqa: F401
     transmission,
     animalito_result,
 )
+from .models import Device
+@admin.register(Device)
+class DeviceAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "branch",
+        "device_id",
+        "activation_code",   # ✅ nueva columna
+        "registered_ip",
+        "is_active",
+        "last_seen",
+    )
+    search_fields = ("device_id", "activation_code", "registered_ip", "branch__name")
+    list_filter = ("is_active", "branch")
