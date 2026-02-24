4dff51c9ed46bf1166de249226ab7888178b7dff
Saul Santeliz <santelizdev@gmail.com>
2026-02-24 12:16:47 -0300

    rollback

--- core/admin.py
+++ core/admin.py
@@ -10,10 +10,3 @@ from .admin_configs import (  # noqa: F401
     transmission,
     animalito_result,
 )
-from django.contrib import admin
-from .models import Device
-
-class DeviceAdmin(admin.ModelAdmin):
-    list_display = ("id","branch","device_id","activation_code","registered_ip","is_active","last_seen")
-
-admin.site.register(Device, DeviceAdmin)
