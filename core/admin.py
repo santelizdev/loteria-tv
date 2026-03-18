bce6b243b76e32b230b2039971750f8131af6cb1
Saul Santeliz <santelizdev@gmail.com>
2026-03-18 19:25:07 -0300

    Fix duplicate AnimalitoArchive admin registration

--- core/admin.py
+++ core/admin.py
@@ -1,7 +1,4 @@
 # core/admin.py
-from django.contrib import admin
-from .models import AnimalitoArchive
-
 from .admin_configs import (  # noqa: F401
     client,
     branch,
@@ -15,12 +12,3 @@ from .admin_configs import (  # noqa: F401
     animalito_result,
     scraper_health,
 )
-
-@admin.register(AnimalitoArchive)
-class AnimalitoArchiveAdmin(admin.ModelAdmin):
-    def get_list_display(self, request):
-        return [f.name for f in self.model._meta.fields]
-
-    def get_list_filter(self, request):
-        fields = {f.name for f in self.model._meta.fields}
-        return tuple([f for f in ("provider", "draw_date") if f in fields])
